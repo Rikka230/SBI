154d247aad9fe67bc38cd97cbc3381d72ee8ad76
Rikka230 <viard.antony83@gmail.com>
2026-05-12 15:07:49 +0200

    Add files via upload

--- public/js/sbi-version.js
+++ public/js/sbi-version.js
@@ -6,12 +6,12 @@
  */
 
 export const SBI_VERSION = {
-  version: '8.0P.106',
+  version: '8.0P.107',
   branch: 'public-pjax-app-shell',
   channel: 'PUBLIC PJAX APP SHELL',
-  stage: 'Mobile formations cleanup',
+  stage: 'Mobile public structural cleanup',
   updatedAt: '2026-05-12',
-  label: 'SBI 8.0P.106 - MOBILE FORMATIONS CLEANUP'
+  label: 'SBI 8.0P.107 - MOBILE PUBLIC STRUCTURAL CLEANUP'
 };
 
 export function getSbiVersionLabel() {
