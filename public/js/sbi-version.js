e5727c6c596e1e4040d21c3afb4413769477a3dc
Rikka230 <viard.antony83@gmail.com>
2026-05-13 18:26:09 +0200

    SBI 8.0P.146 P2F.4 - fix index hiring CTA width

--- public/js/sbi-version.js
+++ public/js/sbi-version.js
@@ -6,12 +6,12 @@
  */
 
 export const SBI_VERSION = {
-  version: '8.0P.145',
+  version: '8.0P.146',
   branch: 'private-admin-accounts-mail-workflow',
-  channel: 'ADMIN MAIL WORKFLOW P2F.3',
-  stage: 'PRIVATE P2F.3 DOMAIN RESET LINK + INDEX CTA FIX',
+  channel: 'ADMIN MAIL WORKFLOW P2F.4',
+  stage: 'PRIVATE P2F.4 INDEX HIRING CTA WIDTH FIX',
   updatedAt: '2026-05-13',
-  label: 'SBI 8.0P.145 - PRIVATE P2F.3 DOMAIN RESET LINK + INDEX CTA FIX'
+  label: 'SBI 8.0P.146 - PRIVATE P2F.4 INDEX HIRING CTA WIDTH FIX'
 };
 
 export function getSbiVersionLabel() {
