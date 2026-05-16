b4618a0ccd05361c637dbf148e3756dcb1ffadee
Rikka230 <viard.antony83@gmail.com>
2026-05-16 19:33:52 +0200

    Fix account escalation HTML escaping

--- public/admin/js/admin-account-escalations-lite.js
+++ public/admin/js/admin-account-escalations-lite.js
@@ -344,6 +344,6 @@ function escapeHtml(value) {
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
-    .replace(/\"/g, '&quot;')
+    .replace(/"/g, '&quot;')
     .replace(/'/g, '&#039;');
 }
